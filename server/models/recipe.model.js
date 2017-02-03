d309ae71b4783b15e31295674c27161ed04b6ee6
Kan Adachi <kadachi1023@gmail.com>
2017-02-03 15:19:57 -0800

    :hammer: adds name_jp field to recipe schema

--- server/models/recipe.model.js
+++ server/models/recipe.model.js
@@ -6,12 +6,15 @@ const Recipe = sequelize.define('recipe', {
   name: {
     type     : Sequelize.STRING,
     allowNull: false,
-    unique   : true,
+  },
+  name_jp: {
+    type: Sequelize.STRING,
   },
   notes: {
     type     : Sequelize.STRING,
     allowNull: false,
   },
+  cook_time: Sequelize.STRING,
 });
 
 export default Recipe;
