d8043f9ca5a63518a11bde6245f356f9d9dbcf77
Kan Adachi <kadachi1023@gmail.com>
2017-02-08 20:25:36 -0800

    :wrench: adds join tables

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,28 @@
+import Sequelize from 'sequelize';
+
+import sequelize from '../db';
 import User from './user.model';
+import Ingredients from './ingredients.model';
+import Recipe from './recipe.model';
+
+// self referential many-to-many for arrangements (derivative recipes)
+Recipe.belongsToMany(Recipe, {
+  as     : 'derivative',
+  through: 'arrangements',
+});
+
+// recipe-ingredients join
+const RecipeIngredients = sequelize.define('RecipeIngredients', {
+  quantity: {
+    type     : Sequelize.INTEGER,
+    allowNull: false,
+  },
+});
+Recipe.belongsToMany(Ingredients, { through: RecipeIngredients });
+Ingredients.belongsToMany(Recipe, { through: RecipeIngredients });
 
 export {
   User,
+  Ingredients,
+  Recipe,
 };
