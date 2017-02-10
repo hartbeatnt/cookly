02ae6b52e4ab279ae85720d9ad65dd9bbdf2c8f5
Kan Adachi <kadachi1023@gmail.com>
2017-02-09 20:12:24 -0800

    :wrench: adds "addRecipe" controller function

---
+++ server/controllers/recipe.controller.js
@@ -0,0 +1,50 @@
+import { isRequestInvalid } from '../services/validation';
+import { Recipe } from '../models';
+
+/**
+ *
+ *  @route /api/recipe/add
+ *
+ *  @method {POST}
+ *
+ *  @body {
+ *    name        : STRING
+ *    ingredients : ARRAY<INT>
+ *    notes       : STRING
+ *    arrangements: ARRAY<INT>
+ *  }
+ */
+export const addRecipe = async (req, res) => {
+  req.checkBody('name', 'Please provide a recipe name').notEmpty();
+  req.checkBody('ingredients', 'Please provide an ingredients list').notEmpty().isArray();
+  req.checkBody('notes', 'Please provide notes').notEmpty();
+  if (await isRequestInvalid(req, res)) {
+    return;
+  }
+
+  try {
+    const { name, ingredients, notes, arrangements } = req.body;
+    let recipe = await Recipe.findOne({ name });
+    if (recipe) {
+      return res.json({
+        success: false,
+        message: `Recipe with name ${name} already exists!`,
+      });
+    }
+    recipe = await Recipe.create({ name, ingredients, notes, arrangements });
+    res.json({
+      success: true,
+      message: 'created recipe',
+      recipe : recipe.values(),
+    });
+  } catch (e) {
+    console.log(e);
+    res.json({
+      success: false,
+      message: `an error occured. e = ${e.toString()}`,
+    });
+  }
+  res.json({
+    success: true,
+  });
+};
