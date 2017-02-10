68e5736a3f882d9ce0e7964d3edb7ad453b9695f
Kan Adachi <kadachi1023@gmail.com>
2017-02-10 15:42:33 -0800

    :hammer: sets up scss modules

---
+++ client/components/navbar/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import './Navbar.scss';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar">
+        <h1>Navbar Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
--- client/index.js
+++ client/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+import 'normalize-css/normalize.css';
+import 'flexboxgrid/dist/flexboxgrid.css';
+
 import './index.scss';
 
 ReactDOM.render(
