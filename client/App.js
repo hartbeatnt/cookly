57da5f2e25cea2ca5c9e227e00bf4f362e03a9ca
Kan Adachi <kadachi1023@gmail.com>
2017-02-14 16:04:56 -0800

    :wrench: adds navbar component & scaffolds basic layout

--- client/App.js
+++ client/App.js
@@ -7,12 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+        <div className="app-body wrapper row">
+          <div className="view col-md-8">
+            {this.props.children}
+          </div>
+          <div className="sidebar col-md-4">
+            <h3>Sidebar</h3>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
